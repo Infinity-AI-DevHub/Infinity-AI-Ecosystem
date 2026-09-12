a223419e2eba01478bf249421fd9b4125b66bee5
Thinula Damsith <107210849+thinula03@users.noreply.github.com>
2026-09-13 03:29:13 +0530

    Web updates

--- apps/web/src/main.tsx
+++ apps/web/src/main.tsx
@@ -2,6 +2,9 @@ import { StrictMode } from 'react'
 import { createRoot } from 'react-dom/client'
 import './index.css'
 import App from './App.tsx'
+import { isDesktop } from './lib/desktop'
+
+if (isDesktop) document.documentElement.dataset.surface = 'desktop'
 
 createRoot(document.getElementById('root')!).render(
   <StrictMode>
